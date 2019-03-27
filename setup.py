49dd008ff6791ad5470f7bb0dbb33c7632ee3de5
Alberto Ortega <a0rtega@users.noreply.github.com>
2019-03-27 18:31:23 +0100

    Update version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name                  = 'metame',
   packages              = ['metame'],
-  version               = '0.1',
+  version               = '0.2',
   description           = 'metame is a metamorphic code engine for arbitrary executables',
   author                = 'Alberto Ortega',
   author_email          = 'aortega.lms+metame@gmail.com',
